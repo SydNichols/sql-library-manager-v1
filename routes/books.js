4c91db32d9fc7dc09304584e96f7d223d66fe9de
SydNichols <sydneynichols.m@gmail.com>
2025-06-01 14:44:06 -0400

    New book form
    Creates new book form route and pug template

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { Book } = require('../models');
 
+//new book form
+router.get('/new', (req, res) => {
+    res.render('new-book', { book: {}, title: 'New Book' });
+});
 
+//render books from list
 router.get('/', async(req, res, next) => {
     try {
         const books = await Book.findAll();
