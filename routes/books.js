3ed06e599ade5787f0c429b6525a1f2b9b59cdef
SydNichols <sydneynichols.m@gmail.com>
2025-06-01 14:51:29 -0400

    New book template and route
    Route added to add book data to book list and render errors for missing fields

--- routes/books.js
+++ routes/books.js
@@ -7,6 +7,25 @@ router.get('/new', (req, res) => {
     res.render('new-book', { book: {}, title: 'New Book' });
 });
 
+//adding books to list
+router.post('/new', async (req, res, next) => {
+    try {
+        const book = await Book.create(req.body);
+        res.redirect('/books');
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError') {
+            const book = await Book.build(req.body);
+            res.render('new-book', {
+                book,
+                errors: error.errors,
+                title: 'New Book'
+            });
+        } else {
+            next(error);
+        }
+    }
+});
+
 //render books from list
 router.get('/', async(req, res, next) => {
     try {
